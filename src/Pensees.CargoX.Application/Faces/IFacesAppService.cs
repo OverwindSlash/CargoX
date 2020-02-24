29c91bdfadb971d9bea74e8819cf2bcf3ff07434
Dai Hao <overwind@outlook.com>
2020-02-24 18:38:14 +0800

    Remove duplicated function

--- src/Pensees.CargoX.Application/Faces/IFacesAppService.cs
+++ src/Pensees.CargoX.Application/Faces/IFacesAppService.cs
@@ -12,8 +12,6 @@ namespace Pensees.CargoX.Faces
     {
         Task<ListResultDto<ClusteringFaceDto>> QueryClusteringFaceByParams(Dictionary<string, Dictionary<string, string>> parameters);
         ClusteringFaceDto TestQueryByParams(Dictionary<string, Dictionary<string,string>> parameters);
-        Task<ListResultDto<ClusteringFaceDto>> QueryClusteringFaceByParams(Dictionary<string, string> parameters);
-
         Task<ListResultDto<ClusteringFaceDto>> QueryClusteringFaceWithContition(string condition);
     }
 }
