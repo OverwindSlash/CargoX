e1abda023c87bc5daf68e4b0bcdb29ebd102cfac
Dai Hao <overwind@outlook.com>
2020-03-03 09:23:49 +0800

    Add ape aps case notification subscribe entity

---
+++ src/Pensees.CargoX.Core/Entities/Cases/CaseInfo.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using Abp.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pensees.CargoX.Entities
+{
+    [DisplayName("视频案件事件对象")]
+    public class CaseInfo : Entity<long>
+    {
+        /// <summary>
+        /// 案事件标识
+        /// CaseObjectIdType::string(30)
+        /// GA/T 1400.1《公安视频图像信息应用系统 第1部分：通用技术要求》，案(事)件管理对象ID 
+        /// </summary>
+        [Required]
+        [StringLength(30)]
+        [DisplayName("案事件标识")]
+        public string CaseID { get; set; }
+
+        /// <summary>
+        /// 案件关联标识
+        /// CaseLinkMarkType::string(23) 案件编号
+        /// </summary>
+        [DisplayName("案件关联标识")]
+        [StringLength(23)]
+        public string CaseLinkMark { get; set; }
+
+        /// <summary>
+        /// 案件名称
+        /// CaseNameType::string(0...120) 
+        /// </summary>
+        [Required]
+        [MaxLength(120)]
+        [DisplayName("案件名称")]
+        public string CaseName { get; set; }
+
+        /// <summary>
+        /// 简要案情
+        /// 案(事)件主要信息描述，表示案件的简要说明
+        /// CaseAbstractType::string(0...4000)
+        /// </summary>
+        [Required]
+        [MaxLength(4000)]
+        [DisplayName("简要案情")]
+        public string CaseAbstract { get; set; }
+
+        /// <summary>
+        /// 线索标识
+        /// 视频标识或者图像标识列表，有多个时用英文半角分号”;”分隔
+        /// </summary>
+        [Required]
+        [MaxLength(1024)]
+        [DisplayName("线索标识")]
+        public string ClueID { get; set; }
+
+        /// <summary>
+        /// 事发时间上限
+        /// </summary>
+        [Required]
+        [DisplayName("事发时间上限")]
+        public DateTime TimeUpLimit { get; set; }
+
+        /// <summary>
+        /// 事发时间下限
+        /// </summary>
+        [DisplayName("事发时间下限")]
+        public DateTime TimeLowLimit { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [DisplayName("创建时间")]
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 事发地点行政区划代码
+        /// PlaceCodeType::string(6)::GB/T 2260，行政区划、籍贯省市县代码
+        /// </summary>
+        [Required]
+        [DisplayName("事发地点行政区划代码")]
+        [StringLength(6)]
+        public string PlaceCode { get; set; }
+
+        /// <summary>
+        /// 事发地点区划内详细地址
+        /// PlaceFullAddressType::string(0...100)
+        /// GA/T 543.1，具体到摄像机位置或街道门牌号，由DE00072(乡镇街道)+ DE00074(街路巷)+ DE00080(门楼牌号)+ DE00081(门楼详细地址)构成
+        /// </summary>
+        [Required]
+        [DisplayName("事发地点区划内详细地址")]
+        [MaxLength(100)]
+        public string PlaceFullAddress { get; set; }
+
+        /// <summary>
+        /// 可疑人数量
+        /// </summary>
+        [DisplayName("可疑人数量")]
+        public int SuspectNumber { get; set; }
+
+        /// <summary>
+        /// 发现人标识符
+        /// 列表形式，多个时以”;”间隔。发现人的人员标识
+        /// </summary>
+        [DisplayName("发现人标识符")]
+        [MaxLength(1024)]
+        public string WitnessIDs { get; set; }
+        
+        /// <summary>
+        /// 创建人姓名
+        /// NameType::string(0..50)
+        /// </summary>
+        [DisplayName("创建人姓名")]
+        [MaxLength(50)]
+        public string CreatorName { get; set; }
+
+        /// <summary>
+        /// 创建人证件类型
+        /// IDType::string(3)::GAT 517，常用证件代码
+        /// 案件入库时创建人的有效证件类型
+        /// </summary>
+        [DisplayName("创建人证件类型")]
+        [StringLength(3)]
+        public string CreatorIDType { get; set; }
+
+        /// <summary>
+        /// 创建人证件号码
+        /// IdNumberType::string(0..30)::证件号码
+        /// 案件入库时创建人的有效证件号码
+        /// </summary>
+        [DisplayName("创建人证件号码")]
+        [StringLength(30)]
+        public string CreatorID { get; set; }
+
+        /// <summary>
+        /// 创建人单位名称
+        /// 案件入库时创建人的所在单位名称
+        /// OrgType::string(0..100)
+        /// </summary>
+        [DisplayName("创建人单位名称")]
+        [MaxLength(100)]
+        public string CreatorOrg { get; set; }
+
+        /// <summary>
+        /// 事发地经度
+        /// LongitudeType::double
+        /// </summary>
+        [DisplayName("事发地经度")]
+        public double Longitude { get; set; }
+
+        /// <summary>
+        /// 事发地纬度
+        /// LatitudeType::double
+        /// </summary>
+        [DisplayName("事发地纬度")]
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// 机动车标识符  列表形式，多个时以”;”间隔
+        /// </summary>
+        [DisplayName("机动车标识符")]
+        [MaxLength(1024)]
+        public string MotorVehicleIDs { get; set; }
+
+        /// <summary>
+        /// 非机动车标识符  列表形式，多个时以”;”间隔
+        /// </summary>
+        [DisplayName("非机动车标识符")]
+        [MaxLength(1024)]
+        public string NonMotorVehicleIDs { get; set; }
+
+        /// <summary>
+        /// 人员标识符  列表形式，多个时以”;”间隔
+        /// </summary>
+        [DisplayName("人员标识符")]
+        [MaxLength(1024)]
+        public string PersonIDs { get; set; }
+
+        /// <summary>
+        /// 人脸标识符  列表形式，多个时以”;”间隔
+        /// </summary>
+        [DisplayName("人脸标识符")]
+        [MaxLength(1024)]
+        public string FaceIDs { get; set; }
+
+        /// <summary>
+        /// 物品标识符  列表形式，多个时以”;”间隔
+        /// </summary>
+        [DisplayName("物品标识符")]
+        [MaxLength(1024)]
+        public string ThingIDs { get; set; }
+
+        /// <summary>
+        /// 文件标识符  列表形式，多个时以”;”间隔
+        /// </summary>
+        [DisplayName("文件标识符")]
+        [MaxLength(1024)]
+        public string FileIDs { get; set; }
+
+        /// <summary>
+        /// 场景标识符  列表形式，多个时以”;”间隔
+        /// </summary>
+        [DisplayName("场景标识符")]
+        [MaxLength(1024)]
+        public string SceneIDs { get; set; }
+
+        /// <summary>
+        /// 相关案件标识  列表形式，多个时以”;”间隔
+        /// </summary>
+        [DisplayName("相关案件标识")]
+        [MaxLength(1024)]
+        public string RelateCaseIdList { get; set; }
+
+        /// <summary>
+        /// 父案件标识  被归并后指向父案件
+        /// CaseObjectIdType::string(30)
+        /// GA/T 1400.1《公安视频图像信息应用系统 第1部分：通用技术要求》，案(事)件管理对象ID 
+        /// </summary>
+        [DisplayName("父案件标识")]
+        [StringLength(30)]
+        public string ParentCaseId { get; set; }
+
+        /// <summary>
+        /// 案件状态
+        /// 0:事件，1：已立案，2：已侦破，3：侦破待复核，4：已结案，5：结案待复核，6：并案待复核、7：撤案待复核，8：结案归档，9：并案归档，10：撤案归档
+        /// </summary>
+        [Required]
+        [DisplayName("案件状态")]
+        public int State { get; set; }
+    }
+}
---
+++ src/Pensees.CargoX.Core/Entities/Subscribes/Notification.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Abp.Domain.Entities;
+
+namespace Pensees.CargoX.Entities
+{
+    public class Notification : Entity<long>
+    {
+        /// <summary>
+        /// 通知标识符 订阅的通知标识符
+        /// </summary>
+        [Required]
+        [StringLength(33)]
+        public string NotificationID { get; set; }
+
+        /// <summary>
+        /// 订阅标识符
+        /// 该订阅标识符，数据共享接口调用时由VIID生成，级联接口调用时不可为空，取消订阅时必选
+        /// </summary>
+        [Required]
+        [StringLength(33)]
+        public string SubscribeID { get; set; }
+
+        /// <summary>
+        /// 订阅标题
+        /// 描述订阅的主题和目标，订阅时必选
+        /// </summary>
+        [Required]
+        [MaxLength(256)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 触发时间  
+        /// </summary>
+        [Required]
+        public DateTime TriggerTime { get; set; }
+
+        /// <summary>
+        /// 信息标识
+        /// 订阅通知的详细信息(人、车、物、场景)标识集合
+        /// </summary>
+        [MaxLength(1024)]
+        public string InfoIDs { get; set; }
+
+        /// <summary>
+        /// 视频案事件  视频案事件信息数据集
+        /// </summary>
+        public List<Case> Cases { get; set; }
+
+        /// <summary>
+        /// 视频卡口	视频卡口信息数据集
+        /// </summary>
+        public List<Tollgate> TollgateList { get; set; }
+
+        /// <summary>
+        /// 车道  车道信息数据集
+        /// </summary>
+        public List<Lane> LaneList { get; set; }
+
+        /// <summary>
+        /// 设备  设备信息数据集
+        /// </summary>
+        public List<Ape> DeviceList { get; set; }
+
+        /// <summary>
+        /// 设备状态  该通知针对批量订阅方式
+        /// </summary>
+        public List<ApeStatus> DeviceStatusList { get; set; }
+
+        /// <summary>
+        /// 采集系统  设备网管信息数据集
+        /// </summary>
+        public List<Aps> APSObjectList { get; set; }
+
+        /// <summary>
+        /// 采集系统状态  该通知针对批量订阅方式
+        /// </summary>
+        public List<ApeStatus> APSStatusObjectList { get; set; }
+
+        /// <summary>
+        /// 人员信息  人员信息数据集
+        /// </summary>
+        public List<Person> PersonObjectList { get; set; }
+
+        /// <summary>
+        /// 人脸信息  人脸信息数据集
+        /// </summary>
+        public List<Face> FaceObjectList { get; set; }
+    }
+}
